72a293b5ac773ee33d7d790d292465192c2448f6
elifnerd <elisabethanne81@gmail.com>
2023-06-20 17:04:33 -0400

    wrote api_view in more concise way

--- cars/views.py
+++ cars/views.py
@@ -11,12 +11,8 @@ def cars_list(request):
         cars = Car.objects.all()
         serializer = CarSerializer(cars, many=True)
         return Response(serializer.data)
-    
-    
     elif request.method == 'POST':
         serializer = CarSerializer(data=request.data)
-        if serializer.is_valid() == True:
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
